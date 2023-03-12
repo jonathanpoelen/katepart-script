852e166bbf441d60dd59f9c77b7f35a422945dc9
Jonathan Poelen <jonathan.poelen@gmail.com>
2023-03-12 17:26:53 +0100

    update test.sh

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ ls -1 \
   ~/projects/kplugins/katepart-script/katepart5/script/commands/*.js \
   ~/projects/kplugins/katepart-script/katepart5/script/libraries/jln/*.js \
   ~/projects/kplugins/katepart-script/tests/*cpp \
-| entr -s 'ninja autotest&& XDG_DATA_DIRS=/home/jonathan/projects/kplugins/katepart-script timeout 3 ./autotest '"${*@Q}"' 2>&1 | sed s/$/$/\;/^kf/d'
+| entr -s 'ninja autotest && set -o pipefail && XDG_DATA_DIRS=/home/jonathan/projects/kplugins/katepart-script timeout 2 ./autotest '"${*@Q}"' 2>&1 | sed s/$/$/\;/^kf/d || { c=$? ; if (( $c == 124 )); then echo -e "\e[31mTimeout\e[m"; fi ; exit $c ; }'
